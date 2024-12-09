adbbe5041930b270ee66148b6fc082c6912d052c
Alain Schaerer <aschaerer@pcatg.com>
2024-12-09 13:59:46 -0500

    Removed hardcoded

--- LlmClientFactory.py
+++ LlmClientFactory.py
@@ -9,7 +9,7 @@ class LlmClientFactory:
     @staticmethod
     def get_client (client_type: ClientType):
         if client_type == ClientType.OPENROUTER:
-            return Client( api_key="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",
+            return Client( api_key="",
                 base_url="https://openrouter.ai/api/v1")
 
         elif client_type == ClientType.HUGGINGFACE:
